a1e711c75c3ac78faeefd3d9ca412b6751f3264f
viveksinra <vivek.kr.vks@gmail.com>
2025-01-30 22:08:25 +0530

    Update URL in config.php for secure panel access

--- includes/config.php
+++ includes/config.php
@@ -1,6 +1,6 @@
 <?php ob_start();
 session_start();
-define("LINK","https://www.insofy.in/secure_panel_adinath/");
+define("LINK","https://www.aftech.in/secure_panel_adinath/");
 
 include_once("secure_panel_adinath/com/sqlConnection.php");
 $db=new sqlConnection();
